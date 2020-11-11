64df5642ea58a512fd66fa468960933b35b8706c
JoshGrant5 <joshgg@icloud.com>
2020-11-10 18:34:12 -0800

    Edited admin dashboard to list count of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  # Display a count of how many products are in the database
+  # Display a count of how many categories are in the database
   def show
+    @product_count = Product.select('name', 'quantity')
+    puts 'PRODUCT COUNT -------', @product_count.inspect
+    @category_count = Category.select('name')
+    puts 'CATEGORY COUNT --------', @category_count.inspect
   end
 end
