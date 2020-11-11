fc5bc778ae94eb2ee6b2a7aca67c81621ebaaec4
JoshGrant5 <joshgg@icloud.com>
2020-11-11 10:39:56 -0800

    Added view for admin categories index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,8 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def index
-
+    names = Category.select('id', 'name')
+    @categories = names.map { |name| { name: name[:name], quantity: Product.where(category_id: name[:id]).count } }
   end
 
   def new
