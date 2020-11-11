a311ca7024f2e4f9646f5fb66d14ef470d1c1f02
JoshGrant5 <joshgg@icloud.com>
2020-11-10 19:05:09 -0800

    Added href's to every product/category listed in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,9 +2,9 @@ class Admin::DashboardController < ApplicationController
   # Display a count of how many products are in the database
   # Display a count of how many categories are in the database
   def show
-    @product_count = Product.select('name', 'quantity')
+    @product_count = Product.select('id', 'name', 'quantity')
     puts 'PRODUCT COUNT -------', @product_count.inspect
-    @category_count = Category.select('name')
+    @category_count = Category.select('id', 'name')
     puts 'CATEGORY COUNT --------', @category_count.inspect
   end
 end
