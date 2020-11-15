68e4a97a621ed8620e1fbbd7b74fadfca50b1d4d
JoshGrant5 <joshgg@icloud.com>
2020-11-15 15:13:54 -0800

    Edited the login page to show a popup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     # If the user exists AND the password entered is correct.
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user = User.authenticate_with_credentials(session_params[:email], session_params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
@@ -14,6 +14,10 @@ class SessionsController < ApplicationController
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
     end
+  end
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 
   def destroy
