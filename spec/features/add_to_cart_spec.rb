c82b0c87f51fd04cf087488dd0c20c1143a97c45
JoshGrant5 <joshgg@icloud.com>
2020-11-15 14:07:34 -0800

    Added add to cart test using capybara

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "clicking 'Add to Cart' button changes  'My Cart (0)' to 'My Cart (1)'" do
+    
+    visit root_path
+    expect(page).to have_css 'article.product', count: 10
+
+    find_button('Add', match: :first).click
+
+    #Debug
+    sleep 1
+    save_screenshot
+
+    expect(page).to have_content 'My Cart (1)'
+    expect(page).to have_link 'My Cart (1)', href: '/cart'
+
+  end
+  
+end
