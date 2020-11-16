65e8ba04490200a0246bc6ba6d10ec332486fef3
JoshGrant5 <joshgg@icloud.com>
2020-11-16 13:21:07 -0800

    Added template for order comfirmation email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
+
+      # Tell the UserMailer to send a welcome email after save
+      UserMailer.welcome_email(user).deliver_now
+      # format.html { redirect_to(user, notice: 'User was successfully created.') }
+      # format.json { render json: user, status: :created, location: user }
+
     else
       redirect_to '/signup'
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,11 @@ class UserMailer < ApplicationMailer
     @url  = '/login'
     mail(to: @user.email, subject: 'Welcome to the Jungle!')
   end
+
+  def order_email(user, order)
+    @user = user
+    @order = order
+    @url = '/'
+    mail(to: @user.email, subject: 'Thank you for your order!')
+  end
 end
