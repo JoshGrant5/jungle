91b5e62fc59e0ff5b6d3695aa289281128be3301
JoshGrant5 <joshgg@icloud.com>
2020-11-17 11:21:01 -0800

    Added review model, migration, and spec files

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
 
+  has_many :reviews
+
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
