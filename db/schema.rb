a2a23fe3aff0af31b82a98e1a0ad617b4267de5e
JoshGrant5 <joshgg@icloud.com>
2020-11-17 11:21:43 -0800

    Added review table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201111232022) do
+ActiveRecord::Schema.define(version: 20201117191641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20201111232022) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "name" 
+    t.string   "name"
     t.text     "description"
     t.string   "image"
     t.integer  "price_cents"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20201111232022) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "rating"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sales", force: :cascade do |t|
     t.string   "name"
